73309fff2bd456b62bae610f492a7a8489dae208
samuel-coutinho <sfcrp2000@gmail.com>
2020-11-27 21:03:26 -0500

    Test if the method returns a valid html - fails

--- spec/elevatormedia_spec.rb
+++ spec/elevatormedia_spec.rb
@@ -6,6 +6,12 @@ describe ElevatorMedia::Streamer do
             it 'returns a string' do
                 expect(ElevatorMedia::Streamer.getContent).to be_a(String)
             end
+
+            it 'the return is a valid HTML' do
+                expect(ElevatorMedia::Streamer.getContent).to start_with("<div>")
+                expect(ElevatorMedia::Streamer.getContent).to end_with("</div>")
+            end
+
         end
     end    
 end
